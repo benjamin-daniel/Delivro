f621a64d08fb6e3d04cf2da1638003bf3820608f
Benjamin Daniel <benjamindaniel706@gmail.com>
2020-06-17 20:38:15 +0100

    added logo to screen

--- src/screens/OnBoarding.jsx
+++ src/screens/OnBoarding.jsx
@@ -1,23 +1,35 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { useOnBoardDispatch } from "contexts";
 import Colors from "constants/Colors";
+import Wobbly from "assets/images/wobbly.svg";
+import Images from "constants/Images";
+
+const LOGO_SIZE = 80;
 
 const OnBoarding = ({ navigation }) => {
   const dispatch = useOnBoardDispatch();
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: Colors.tintColor }}>
+    <View style={{ flex: 1, backgroundColor: Colors.tintColor }}>
+      <View style={{ height: 300 }}>
+        <Wobbly style={{ position: "absolute" }} />
+        <Image style={styles.logo} source={Images.logo} />
+      </View>
       <View>
         <Text>Hey</Text>
         <TouchableOpacity onPress={() => dispatch({ type: "board" })}>
           <Text>Next</Text>
         </TouchableOpacity>
       </View>
-    </SafeAreaView>
+    </View>
   );
 };
 
 export default OnBoarding;
+
+const styles = StyleSheet.create({
+  logo: { height: LOGO_SIZE, width: LOGO_SIZE, marginTop: 60 },
+});
